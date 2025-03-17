17c4590ca4e6ebbb386f9d56cbcf0f8a6fc7b80b
Yahya Al-Shamali <42281247+PointlessUser@users.noreply.github.com>
2025-03-17 16:20:19 -0600

    Update build.sh

--- conda_recipe/build.sh
+++ conda_recipe/build.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 mkdir -p build
 cd build
-cmake \
-  -DCMAKE_C_COMPILER="${CC}" \
-  -DCMAKE_CXX_COMPILER="${CXX}" \
-  -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
-  -DCMAKE_OSX_DEPLOYMENT_TARGET=${MACOSX_DEPLOYMENT_TARGET} \
-  -DCMAKE_SYSROOT="${CONDA_BUILD_SYSROOT}" \
-  -DCMAKE_C_FLAGS="${CFLAGS} -DHAVE_STRDUP=1" \
-  -DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
-  ..
-
+cmake -DCMAKE_CXX_COMPILER=${CXX} -DCMAKE_C_FLAGS="-DHAVE_STRDUP=1" ..
 cmake --build . --parallel
 cmake --install . --prefix=${PREFIX}
