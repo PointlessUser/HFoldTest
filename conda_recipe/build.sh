a8ad59edb6300a727b7af91af587d26c5c4a9854
Yahya Al-Shamali <42281247+PointlessUser@users.noreply.github.com>
2025-03-19 13:42:36 -0600

    Update build.sh

--- conda_recipe/build.sh
+++ conda_recipe/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+set -ex  # <--- ADD THIS! Enables bash debug + exit on error
+
 mkdir -p build
 cd build
-cmake -DCMAKE_CXX_COMPILER=${CXX} -DCMAKE_C_FLAGS="-DHAVE_STRDUP=1" ..
+cmake -DCMAKE_CXX_COMPILER=${CXX} -DCMAKE_C_FLAGS="-DHAVE_STRDUP=1" -DCMAKE_BUILD_TYPE=Release -DCMAKE_VERBOSE_MAKEFILE=ON ..
 cmake --build . --parallel
 cmake --install . --prefix=${PREFIX}
