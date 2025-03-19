87d69bcf74d5d725e12fbe1a1f60cceaac2befef
Yahya Al-Shamali <42281247+PointlessUser@users.noreply.github.com>
2025-03-19 11:17:24 -0600

    Update bld.bat

--- conda_recipe/bld.bat
+++ conda_recipe/bld.bat
@@ -1,15 +1,14 @@
 @echo off
-REM Create build directory
 mkdir build
 cd build
 
-REM Configure with CMake (MSVC will be auto-used)
-cmake -DCMAKE_C_FLAGS="/DHAVE_STRDUP=1" -DCMAKE_INSTALL_PREFIX=%PREFIX% ..
+:: Use Ninja instead of Visual Studio
+cmake -G "Ninja" ^
+  -DCMAKE_C_FLAGS="/DHAVE_STRDUP=1" ^
+  -DCMAKE_INSTALL_PREFIX=%PREFIX% ^
+  %SRC_DIR%
 
-REM Build
 cmake --build . --parallel
-
-REM Install
 cmake --install . --prefix %PREFIX%
 
 cd ..
